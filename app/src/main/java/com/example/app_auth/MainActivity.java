74e20fa097f916dc9b7a357ef3000c0e29ee7ee6
RobertoSuarez <electrosonix12@gmail.com>
2021-12-14 11:27:54 -0500

    petición http se envia correctamente

--- app/src/main/java/com/example/app_auth/MainActivity.java
+++ app/src/main/java/com/example/app_auth/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.app_auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getData();
+    }
+
+    private void getData() {
+        TextView textResponse = (TextView)findViewById(R.id.textResponse);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://gorest.co.in/public/v1/users";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                textResponse.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textResponse.setText("oh oh, a pasado un error");
+            }
+        });
+
+        queue.add(stringRequest);
+
     }
 }
